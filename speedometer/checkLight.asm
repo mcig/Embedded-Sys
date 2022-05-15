b143b38320fbc7ab4acb429f6f8818ab4bb2a1d4
Mustafa Cig Gokpinar <cig-mustafa.2001@hotmail.com>
2022-05-15 20:21:51 +0300

    fixed wrong port bug

--- speedometer/checkLight.asm
+++ speedometer/checkLight.asm
@@ -1,6 +1,8 @@
 .text 
 .global checkLight
 
+.EQU DDRC,  0x07       
+
 ; Analog 2 Digital Converter Regs
 .EQU ADCL,  0x0078
 .EQU ADCH,  0x0079
@@ -8,26 +10,35 @@
 .EQU ADMUX, 0x007C   
 .EQU ADSC,  6
 
+.equ STOP, 0
+.equ CLOCKWISE, 1
+.equ COUNTERCLOCKWISE, 2
+
+; Parameters: None
+; Return integer: light Value
+
 checkLight:
 	; Analog IO Inits
-  	LDI R16, 0B00000000
-  	OUT DDRC, R16
+  	LDI r18, 0B00000000
+  	OUT DDRC, r18
 
-    LDI R16, 0B10000111
-  	STS ADCSRA, R16
+    LDI r18, 0B10000111
+  	STS ADCSRA, r18
 
 	; Analog2Digital Staff 
- 	LDI R16, 0B01000100
-  	STS ADMUX, R16
+ 	LDI r18, 0B01000010
+  	STS ADMUX, r18
 
-	LDS R16, ADCSRA
-    ORI R16, 0x40
-    STS ADCSRA, R16
+	LDS r18, ADCSRA
+    ORI r18, 0x40
+    STS ADCSRA, r18
 
 POLL_ANALOG_READ:
-	LDS R16, ADCSRA
-	SBRC R16, ADSC
+	LDS r18, ADCSRA
+	SBRC r18, ADSC
     RJMP POLL_ANALOG_READ
 
-    LDS R17, ADCH
-	; end Analog2Digital staff
+    LDS r24, ADCL ; maxVal 0xFF
+    LDS r25, ADCH ; maxVal 0x03
+
+	ret
